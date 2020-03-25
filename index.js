e96455ba4b98c52b1786b993de6a8b6a02c15906
Reid Watson <j.watsonreid@gmail.com>
2020-03-25 18:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+// shout to to upper case 
 function shout(string) {
   return string.toUpperCase()
 }
+
+
+// whisper to lower case
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+// logShout
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+
+// logWhisper
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+//say hi to Grandma: 1) returns "I can't hear you" if string is lower case 
+//    
